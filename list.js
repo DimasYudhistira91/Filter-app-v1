5e82e8a549d6083ace67c8facb66e75f754eb6fc
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-07-28 18:19:42 +0700

    perubahan ion brand

--- list.js
+++ list.js
@@ -1,6 +1,5 @@
 var rowDOM = document.getElementById('row1');
 
-
 var judulDOM = document.getElementById('judul-film');
 var tahunDOM = document.getElementById('tahun-film');
 var genreDOM = document.getElementById('genre-film');
@@ -9,13 +8,11 @@ var reviewDOM = document.getElementById('review-film');
 var allList = localStorage.getItem('daftar-film');
 var allListJson = JSON.parse(allList);
 
-console.log(allListJson[0].judul);
-
-rowDOM = allListJson.length;
+console.log(allListJson);
 
 for(i = 0; i < allListJson.length; i++) {
-  judulDOM.innerHTML = allListJson[i].judul;
-  tahunDOM.innerHTML = allListJson[i].tahun;
-  genreDOM.innerHTML = allListJson[i].genre;
-  reviewDOM.innerHTML = allListJson[i].review;
+rowDOM.length = allListJson.length;
+rowDOM.innerHTML += '<th id="judul-film" scope="row">' + allListJson[i].judul + '</th><td id="tahun-film">' + allListJson[i].tahun + '</td><td id="genre-film">' + allListJson[i].genre + '</td><td id="review-film">' + allListJson[i].review + '</td>';
+
 }
+
