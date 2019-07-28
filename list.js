81a399f90ba6d76f8d33fdccae927945d71dfd2d
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-07-28 20:06:43 +0700

    Mengubah parent untuk mencetak data localstorage ke tabel

--- list.js
+++ list.js
@@ -6,12 +6,15 @@ var genreDOM = document.getElementById('genre-film');
 var reviewDOM = document.getElementById('review-film');
 
 var allList = localStorage.getItem('daftar-film');
+
 var allListJson = JSON.parse(allList);
 
 console.log(allListJson);
 
-for(i = 0; i < allListJson.length; i++) {
-rowDOM.length = allListJson.length;
-rowDOM.innerHTML += '<th id="judul-film" scope="row">' + allListJson[i].judul + '</th><td id="tahun-film">' + allListJson[i].tahun + '</td><td id="genre-film">' + allListJson[i].genre + '</td><td id="review-film">' + allListJson[i].review + '</td>';
+if(allListJson.length > 0) {
+
+for(i in allListJson) {
+  rowDOM.innerHTML += '<tr><th id="judul-film" scope="row">' + allListJson[i].judul + '</th><td id="tahun-film">' + allListJson[i].tahun + '</td><td id="genre-film">' + allListJson[i].genre + '</td><td id="review-film">' + allListJson[i].review + '</td></tr>';
 }
 
+}
